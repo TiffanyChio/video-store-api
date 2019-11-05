caa27d77dd4a3909eb0b3530b5bcfba07723d287
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-11-05 14:40:32 -0800

    added customer testing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,63 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    let(:customer){customers(:bob)}
+    
+    it "must have a name" do
+      customer.name = nil
+      
+      refute customer.valid?
+      expect(customer.errors.messages).must_include :name
+      expect(customer.errors.messages[:name]).must_include "can't be blank"
+    end
+    
+    it "must have a registration date" do
+      customer.registered_at = nil
+      
+      refute customer.valid?
+      expect(customer.errors.messages).must_include :registered_at
+      expect(customer.errors.messages[:registered_at]).must_include "can't be blank"
+    end
+    
+    it "must have an address" do
+      customer.address = nil
+      
+      refute customer.valid?
+      expect(customer.errors.messages).must_include :address
+      expect(customer.errors.messages[:address]).must_include "can't be blank"
+    end
+    
+    it "must have a city" do
+      customer.city = nil
+      
+      refute customer.valid?
+      expect(customer.errors.messages).must_include :city
+      expect(customer.errors.messages[:city]).must_include "can't be blank"
+    end
+    
+    it "must have a state" do
+      customer.state = nil
+      
+      refute customer.valid?
+      expect(customer.errors.messages).must_include :state
+      expect(customer.errors.messages[:state]).must_include "can't be blank"
+    end
+    
+    it "must have a postal code" do
+      customer.postal_code = nil
+      
+      refute customer.valid?
+      expect(customer.errors.messages).must_include :postal_code
+      expect(customer.errors.messages[:postal_code]).must_include "can't be blank"
+    end
+    
+    it "must have a phone number" do
+      customer.phone = nil
+      
+      refute customer.valid?
+      expect(customer.errors.messages).must_include :phone
+      expect(customer.errors.messages[:phone]).must_include "can't be blank"
+    end
+  end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -3,27 +3,32 @@ require "test_helper"
 describe Movie do
   describe "validations" do
     let(:movie){movies(:one)}
+    
+    it "can be valid" do
+      assert movie.valid?
+    end
+    
     it "must have a title" do
       movie.title = ""
       refute movie.valid?
       expect(movie.errors.messages).must_include :title
       expect(movie.errors.messages[:title]).must_include "can't be blank"
     end
-
+    
     it "must have a overview" do
       movie.overview = ""
       refute movie.valid?
       expect(movie.errors.messages).must_include :overview
       expect(movie.errors.messages[:overview]).must_include "can't be blank"
     end
-
+    
     it "must have a release date" do 
       movie.release_date = ""
       refute movie.valid?
       expect(movie.errors.messages).must_include :release_date
       expect(movie.errors.messages[:release_date]).must_include "can't be blank"
     end
-
+    
     it "must have an inventory" do
       movie.inventory = nil
       refute movie.valid?
