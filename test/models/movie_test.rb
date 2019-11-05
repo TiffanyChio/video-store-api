1f729b94130894a9519771a5dac1ee970b659e46
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-11-05 14:28:32 -0800

    added movie model validation tests and passed

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe Movie do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    let(:movie){movies(:one)}
+    it "must have a title" do
+      movie.title = ""
+      refute movie.valid?
+      expect(movie.errors.messages).must_include :title
+      expect(movie.errors.messages[:title]).must_include "can't be blank"
+    end
+
+    it "must have a overview" do
+      movie.overview = ""
+      refute movie.valid?
+      expect(movie.errors.messages).must_include :overview
+      expect(movie.errors.messages[:overview]).must_include "can't be blank"
+    end
+
+    it "must have a release date" do 
+      movie.release_date = ""
+      refute movie.valid?
+      expect(movie.errors.messages).must_include :release_date
+      expect(movie.errors.messages[:release_date]).must_include "can't be blank"
+    end
+
+    it "must have an inventory" do
+      movie.inventory = nil
+      refute movie.valid?
+      expect(movie.errors.messages).must_include :inventory
+      expect(movie.errors.messages[:inventory]).must_include "can't be blank"
+    end
+  end
 end
